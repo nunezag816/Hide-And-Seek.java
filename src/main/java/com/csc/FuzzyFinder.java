7377c375a76e63e1627046727bc134235dda83a9
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-03-13 12:41:58 -0500

    Run gradle init

--- src/main/java/com/csc/FuzzyFinder.java
+++
@@ -1,8 +0,0 @@
-package com.csc;
-
-public class FuzzyFinder {
-  // Your code goes here!
-  public static void main(String args[]) {
-    System.out.println("In addition to your search functions, you can write a main function if you wish!");
-  }
-}
