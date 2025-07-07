c4181436f88916684bd532927fbd23a600d115eb
nunezag816 <nunezag@msoe.edu>
2025-07-06 21:01:28 -0500

    Create Fuzzy1.java

---
+++ app/src/main/java/org/example/Fuzzy1.java
@@ -0,0 +1,12 @@
+public class Fuzzy1 implements Feeling {
+    public String color;
+
+    public Fuzzy(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String description() {
+        return color;
+    }
+}
